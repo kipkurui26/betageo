602be3df880f50eb550e276f80338308f4d091cc
kipkurui26 <6mutaibartai58@gmail.com>
2024-06-25 16:24:45 +0300

    Beta Geo-Consultants

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -41,22 +41,22 @@ const NavBar = () => {
 
           <ul className="nav__socials">
             <li className="nav__social">
-              <Link to="" target="_blank" className="social--link">
+              <Link to="#" target="_blank" className="social--link">
                 <FaFacebookF />
               </Link>
             </li>
             <li className="nav__social">
-              <Link to="" target="_blank" className="social--link">
+              <Link to="#" target="_blank" className="social--link">
                 <FaTiktok />
               </Link>
             </li>
             <li className="nav__social">
-              <Link to="" target="_blank" className="social--link">
+              <Link to="#" target="_blank" className="social--link">
                 <FaWhatsapp />
               </Link>
             </li>
             <li className="nav__social">
-              <Link to="" target="_blank" className="social--link">
+              <Link to="#" target="_blank" className="social--link">
                 <FaLinkedinIn />
               </Link>
             </li>
